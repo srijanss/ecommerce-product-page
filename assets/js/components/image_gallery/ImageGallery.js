87602f937c746154c07334a1ffb40dee17bcceb4
Srijan Manandhar <srijan.manandhar@gmail.com>
2024-08-29 09:30:55 +0545

    Added image gallery component and add previous/next functionality

--- assets/js/components/image_gallery/ImageGallery.js
+++ assets/js/components/image_gallery/ImageGallery.js
@@ -1,17 +1,17 @@
 import css from "./ImageGallery.css?inline";
-import PreviousIcon from "../../../images/icon-previous.svg";
-import NextIcon from "../../../images/icon-next.svg";
 import Store from "../../store.js";
 
 export default class ImageGalleryComponent extends HTMLElement {
   constructor() {
     super();
     this.product = Store.product;
+    this.productImagePosition = 0;
   }
 
   connectedCallback() {
     this.shadow = this.attachShadow({ mode: "open" });
     this.render();
+    this.handleEvents();
   }
 
   render() {
@@ -19,36 +19,80 @@ export default class ImageGalleryComponent extends HTMLElement {
       <style>
         ${css}
       </style>
-      <section>
-        <button id="icon-previous">
-          <img src="${PreviousIcon}" alt="Previous" />
-        </button>
-        ${this.product.product_images
-          .map(
-            (productImage) => `
-            <figure>
-              <img src="${productImage.main}" alt="" />
-              <figcaption>${productImage.alt}</figcaption>
-            </figure>
-          `
-          )
-          .join("")}
-        <button id="icon-next">
-          <img src="${NextIcon}" alt="Next" />
-        </button>
-        ${this.product.product_images
-          .map(
-            (productImage, index) => `
-            <button id="product-thumbnail-${index + 1}">
-              <img
-                src="${productImage.thumbnail}"
-                alt="Product thumbnail ${index + 1}"
-              />
-            </button>
-          `
-          )
-          .join("")}
-      </section>
+      <article id="image-gallery" aria-label="Product image gallery">
+        <section id="product-image-list" aria-label="Product images">
+          <button id="previous-btn" aria-label="Previous">
+            <svg width="12" height="18" viewBox="0 0 12 18" xmlns="http://www.w3.org/2000/svg" aria-lablledby="previous-svg-icon" focusable="false" aria-hidden="true">
+              <title id="previous-svg-icon">Previous</title>
+              <path d="M11 1 3 9l8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd"/>
+            </svg>
+          </button>
+          <div id="product-image-container">
+            ${this.product.product_images
+              .map(
+                (productImage, index) => `
+                <button data-productid="${index}" class="product-image-btn">
+                  <img src="${productImage.main}" alt="${productImage.alt}" />
+                </button>
+              `
+              )
+              .join("")}
+          </div>
+          <button id="next-btn" aria-label="Next">
+            <svg width="13" height="18" viewBox="0 0 13 18" xmlns="http://www.w3.org/2000/svg" aria-labelledby="next-svg-icon" aria-hidden="true" focusable="false">
+              <title id="next-svg-icon">Next</title>
+              <path d="m2 1 8 8-8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd"/>
+            </svg>
+          </button>
+        </section>
+        <section id="product-thumbnail-list" aria-label="Product thumbnails">
+          ${this.product.product_images
+            .map(
+              (productImage, index) => `
+              <button data-productid="${index}" class="product-thumbnail-btn">
+                <img
+                  src="${productImage.thumbnail}"
+                  alt="${productImage.alt}"
+                />
+              </button>
+            `
+            )
+            .join("")}
+        </section>
+      </article>
     `;
+  }
+
+  handleEvents() {
+    const productImageContainer = this.shadow.getElementById(
+      "product-image-container"
+    );
+    const previousBtn = this.shadow.getElementById("previous-btn");
+    previousBtn.addEventListener("click", (e) => {
+      if (this.productImagePosition === 0) {
+        this.productImagePosition = 0;
+      } else {
+        this.productImagePosition -= 1;
+      }
+      console.log(this.productImagePosition);
+      productImageContainer.setAttribute(
+        "style",
+        `--translate-x: ${this.productImagePosition}`
+      );
+    });
+
+    const nextBtn = this.shadow.getElementById("next-btn");
+    nextBtn.addEventListener("click", (e) => {
+      if (this.productImagePosition === 3) {
+        this.productImagePosition = 3;
+      } else {
+        this.productImagePosition += 1;
+      }
+      console.log(this.productImagePosition);
+      productImageContainer.setAttribute(
+        "style",
+        `--translate-x: ${this.productImagePosition}`
+      );
+    });
   }
 }
